3da29ce00ffa90573d920334992f305f64dc49a5
Sophia <me@sophia-dev.io>
2023-06-28 12:38:59 +0900

    build: make nextjs built output to be standalone to reduce image size

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: "standalone",
 
     webpack(config) {
         // Grab the existing rule that handles SVG imports
